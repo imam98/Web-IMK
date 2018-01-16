b072fbde9c29e601082c1420bd855a1be624b782
Imam Nududdin <imannududin@outlook.com>
2018-01-17 05:23:12 +0700

    Add contents

--- assets/js/web-components.js
+++ assets/js/web-components.js
@@ -1,3 +1,4 @@
+/* Mobile menu dropdown */
 function dropdownClick() {
   let menu = document.getElementById("mobileMenu")
 
@@ -5,4 +6,17 @@ function dropdownClick() {
     menu.className += " is-showed";
   else
     menu.className = "mobile-menu";
+}
+
+/* Slider */
+function sliderInit() {
+  $('.slider-container').unslider({
+    delay: 5000,
+    autoplay: true,
+    infinite: true,
+    arrows: {
+      prev: '<a class="unslider-arrow prev" href="javascript:void(0);"><i class="fa fa-angle-left"></i></a>',
+      next: '<a class="unslider-arrow next" href="javascript:void(0);"><i class="fa fa-angle-right"</i></a>'
+    }
+  });
 }
